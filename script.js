f8355287acfd77c3c85aeb4bd088ae4395767cf5
Rayyan365 <rayanabdallah09@gamil.com>
2023-08-19 07:07:55 -0700

    remove-duplicated assignment done

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // Good Luck 💪🏾
+function removeDuplicates() {
+ 
+    names = [
+        "hamda","aisha","salman","ubayb", "hamda"
+       
+    ];
+ 
+    jsonObject = names.map(JSON.stringify);
+    uniqueSet = new Set(jsonObject);
+    uniqueArray = Array.from(uniqueSet).map(JSON.parse);
+ 
+    console.log(uniqueArray);
+}
+removeDuplicates();
